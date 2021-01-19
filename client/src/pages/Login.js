292719957f1f4824a98edc79617e77b6c74c0421
Anna Kaser <kaserann96@gmail.com>
2021-01-18 19:53:31 -0700

    home, and login page foundation

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Auth from "../utils/auth";
+
+const Login = () => {
+  return (
+    <div className="container">
+     
+    </div>
+  );
+};
+
+export default Login;
