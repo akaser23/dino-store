5ac1e9f6a89d5783012e72a2e24ac2ec300bf828
Jordan Roberts <jdrobs19@gmail.com>
2021-01-21 21:34:07 -0700

    started footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+            <p>&#169; 2021 Dinostore, LLC</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
