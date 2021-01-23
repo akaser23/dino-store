868d12a9280f07e5d738e99f60e8a3443c52fbae
Anna Kaser <kaserann96@gmail.com>
2021-01-23 10:59:43 -0700

    product list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from './components/Header';
+import ProductList from './components/Listings';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductList />
       <Footer />
     </div>
   );
