d3a389fc43146c7a8747e0b07288fa48ea943df9
Anna Kaser <kaserann96@gmail.com>
2021-01-23 20:32:47 -0700

    listings correction

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import ApolloClient from 'apollo-boost';
 
 import Footer from "./components/Footer";
 import Header from './components/Header';
-import ProductList from './components/Listings';
+import Listings from './components/Listings';
 import CategoryMenu from './components/CategoryMenu';
 
 const client = new ApolloClient({
@@ -26,8 +26,8 @@ function App() {
       <div className="App">
         <Header />
         <StoreProvider>
-        <CategoryMenu />
-          <ProductList />
+          <CategoryMenu />
+          <Listings />
         </StoreProvider>
         <Footer />
       </div>
