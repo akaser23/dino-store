7bd6bb9633ac30db602559e88b8514b71d2a631f
Anna Kaser <kaserann96@gmail.com>
2021-01-24 09:57:44 -0700

    feature/product-list

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -49,6 +49,7 @@ function CategoryMenu() {
             <h2>Choose a Category:</h2>
             {categories.map(item => (
                 <button
+                    // type="link"
                     key={item._id}
                     onClick={() => {
                         handleClick(item._id);
